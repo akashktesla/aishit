85f3f5490df6db161d606778ce298d5066114c1f
Akash tesla <68317027+akashktesla@users.noreply.github.com>
2020-10-27 15:38:03 +0530

    Add files via upload

--- decision.py
+++ decision.py
@@ -1,8 +1,9 @@
 class decision:
-    def __init__(self,h,s,f,a,su,d):
+    def __init__(self,h,s,f,a,su,d,name):
         self.h = h
         self.s = s
         self.f = f
         self.a = a
         self.su = su
         self.d = d
+        self.name=name
