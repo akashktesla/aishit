d33ab045b8d6cf1f8e9bd8c96af381be99f2d794
Akash tesla <68317027+akashktesla@users.noreply.github.com>
2021-02-06 09:07:20 +0530

    Delete decision.py

--- decision.py
+++
@@ -1,9 +0,0 @@
-class decision:
-    def __init__(self,h,s,f,a,su,d,name):
-        self.h = h
-        self.s = s
-        self.f = f
-        self.a = a
-        self.su = su
-        self.d = d
-        self.name=name
